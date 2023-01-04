bd15ab430af6cf147cd05ce8b87848199a758914
Shashank <pshashank1729@gmail.com>
2023-01-04 16:49:47 +0530

    second commit

--- src/context/github/GithubActions.jsx
+++ src/context/github/GithubActions.jsx
@@ -11,17 +11,19 @@ const github = axios.create({
 
 // search users
 export const searchUsers = async (text) => {
-  const params = new URLSearchParams({ q: text });
+  const params = new URLSearchParams({
+    q: text,
+  });
+
   const response = await github.get(`/search/users?${params}`);
   return response.data.items;
 };
 
 // get user and all his repos repos
 export const getUserAndRepos = async (id) => {
-  const params = new URLSearchParams({ sort: "created", per_page: 10 });
   const [user, repos] = await Promise.all([
     github.get(`/users/${id}`),
-    github.get(`/users/${id}/repos?${params}`),
+    github.get(`/users/${id}/repos`),
   ]);
   return {
     user: user.data,
